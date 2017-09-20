ab0c85963a686bdacabe47b5f51b77454d8e191e
albertmas <almaco_io@hotmail.es>
2017-09-20 10:34:16 +0200

    1

---
+++ Physics2_class1_handout/ModulePhysics.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+#define GRAVITY_X 0.0f
+#define GRAVITY_Y -7.0f
+
+#define PIXELS_PER_METER 50.0f
+
+class b2World;
+
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+private:
+	b2World *world;
+	bool debug;
+};
